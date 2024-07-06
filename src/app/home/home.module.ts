b53dd974e24725d8a1cbd40b197de46b7d738f6a
mostafa farrag <mostafa.kz@outlook.sa>
2024-07-06 11:51:30 +0100

    update home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { UserFormComponent } from '../shared/components/user-form/user-form.component';
+import { AudioRecorderService } from '../shared/services/audio-recorder.service';
+import { CameraComponent } from '../shared/components/camera/camera.component';
+import { AudioRecordingComponent } from '../shared/components/audio-recording/audio-recording.component';
+import { NetworkService } from '../shared/services/network.service';
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, UserFormComponent, CameraComponent, AudioRecordingComponent],
+  providers: [AudioRecorderService,],
+  schemas: [ NO_ERRORS_SCHEMA]
 })
-export class HomePageModule {}
+export class HomePageModule { }
